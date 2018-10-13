d746e090956ffde4e78b95bca51b07a8abc5146c
Seva Safris <seva@safris.org>
2018-10-13 15:57:21 +0700

    import static Assert

--- src/test/java/org/fastjax/logging/ColorConverterTest.java
+++ src/test/java/org/fastjax/logging/ColorConverterTest.java
@@ -2,7 +2,7 @@
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
- * in the Software withthis.converter.transform(this.event, this.in) restriction, including withthis.converter.transform(this.event, this.in) limitation the rights
+ * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
@@ -16,9 +16,10 @@
 
 package org.fastjax.logging;
 
+import static org.junit.Assert.*;
+
 import java.util.Collections;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -43,66 +44,66 @@ public class ColorConverterTest {
   @Test
   public void red() {
     this.converter.setOptionList(Collections.singletonList("red"));
-    Assert.assertEquals("\033[0;31min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;31min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void green() {
     this.converter.setOptionList(Collections.singletonList("green"));
-    Assert.assertEquals("\033[0;32min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;32min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void yellow() {
     this.converter.setOptionList(Collections.singletonList("yellow"));
-    Assert.assertEquals("\033[0;33min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;33min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void blue() {
     this.converter.setOptionList(Collections.singletonList("blue"));
-    Assert.assertEquals("\033[0;34min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;34min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void magenta() {
     this.converter.setOptionList(Collections.singletonList("magenta"));
-    Assert.assertEquals("\033[0;35min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;35min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void cyan() {
     this.converter.setOptionList(Collections.singletonList("cyan"));
-    Assert.assertEquals("\033[0;36min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;36min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void faint() {
     this.converter.setOptionList(Collections.singletonList("faint"));
-    Assert.assertEquals("\033[2;39min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[2;39min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void highlightError() {
     this.event.setLevel(Level.ERROR);
-    Assert.assertEquals("\033[0;31min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;31min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void highlightWarn() {
     this.event.setLevel(Level.WARN);
-    Assert.assertEquals("\033[0;33min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;33min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void highlightDebug() {
     this.event.setLevel(Level.DEBUG);
-    Assert.assertEquals("\033[0;39min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;39min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 
   @Test
   public void highlightTrace() {
     this.event.setLevel(Level.TRACE);
-    Assert.assertEquals("\033[0;39min\033[0;39m", this.converter.transform(this.event, this.in));
+    assertEquals("\033[0;39min\033[0;39m", this.converter.transform(this.event, this.in));
   }
 }
