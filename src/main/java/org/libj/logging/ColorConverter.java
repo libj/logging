7bcfd7b94ac4f0175a60098a556fd92eec3d9d3f
Seva Safris <seva@safris.org>
2020-05-22 20:54:04 +0700

    Typographic edit

--- src/main/java/org/libj/logging/ColorConverter.java
+++ src/main/java/org/libj/logging/ColorConverter.java
@@ -34,7 +34,15 @@ public class ColorConverter extends CompositeConverter<ILoggingEvent> {
    * Enum of standard ANSI color codes.
    */
   private enum AnsiColor {
-    BLACK("0"), RED("1"), GREEN("2"), YELLOW("3"), BLUE("4"), MAGENTA("5"), CYAN("6"), WHITE("7"), DEFAULT("9");
+    BLACK("0"),
+    RED("1"),
+    GREEN("2"),
+    YELLOW("3"),
+    BLUE("4"),
+    MAGENTA("5"),
+    CYAN("6"),
+    WHITE("7"),
+    DEFAULT("9");
 
     private final String code;
 
